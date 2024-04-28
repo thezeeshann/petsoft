adf02b8ea341dd7ffa630bbd8746b1c36ec158c2
thezeeshann <info.zeeshann@gmail.com>
2024-04-28 22:34:37 +0530

    refactor: update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        remotePatterns:[
+            {
+                protocol: 'https',
+                hostname: 'bytegrad.com',
+    
+            }
+        ]
+    }
+};
 
 export default nextConfig;
