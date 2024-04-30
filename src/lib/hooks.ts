b068e86573de96eeac68215fb824f8f741a2abf9
thezeeshann <info.zeeshann@gmail.com>
2024-04-30 23:30:52 +0530

    feat: Added custom hooks & types

---
+++ src/lib/hooks.ts
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import { PetContext } from "@/contexts/pet-context-provider";
+import { SearchContext } from "@/contexts/search-context-provider";
+
+export function usePetContext() {
+  const context = useContext(PetContext);
+  if (!context) {
+    throw new Error("usePetContext must be used within a PetContextProvider");
+  }
+  return context;
+}
+
+export function useSearchContext() {
+  const context = useContext(SearchContext);
+  if (!context) {
+    throw new Error(
+      "useSearchContext must be used within a SearchContextProvider"
+    );
+  }
+  return context;
+}
