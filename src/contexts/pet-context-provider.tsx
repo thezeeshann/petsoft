f21f9cca814ca504fb278cd88fa529dded4dd77a
thezeeshann <info.zeeshann@gmail.com>
2024-04-30 23:31:32 +0530

    feat: Added context api

---
+++ src/contexts/pet-context-provider.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import React, { useState, createContext } from "react";
+import { Pet } from "@/lib/types";
+
+type PetContextProviderProps = {
+  children: React.ReactNode;
+  data: Pet[];
+};
+
+type TPetContext = {
+  pets: Pet[];
+  selectedPetId: string | null;
+  selectedPet: Pet | undefined;
+  numberOfPets: number;
+  handleChangeSelectedPetId: (id: string) => void;
+};
+
+export const PetContext = createContext<TPetContext | null>(null);
+
+const PetContextProvider = ({ data, children }: PetContextProviderProps) => {
+  const [pets, setPets] = useState(data);
+  const [selectedPetId, setSelectedPetId] = useState<string | null>(null);
+
+  const selectedPet = pets.find((pet) => pet.id === selectedPetId);
+  const numberOfPets = pets.length;
+
+  const handleChangeSelectedPetId = (id: string) => {
+    setSelectedPetId(id);
+  };
+
+  return (
+    <PetContext.Provider
+      value={{
+        pets,
+        selectedPetId,
+        selectedPet,
+        numberOfPets,
+        handleChangeSelectedPetId,
+      }}
+    >
+      {children}
+    </PetContext.Provider>
+  );
+};
+
+export default PetContextProvider;
---
+++ src/contexts/search-context-provider.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import React, { useState, createContext } from "react";
+
+type SearchContextProviderProps = {
+  children: React.ReactNode;
+};
+
+type TSeachContext = {
+    searchQuery: string;
+    handleChangeSearchQuery: (newValue: string) => void;
+};
+
+export const SearchContext = createContext<TSeachContext | null>(null);
+
+const SearchContextProvider = ({children }: SearchContextProviderProps) => {
+    
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const handleChangeSearchQuery = (newValue:string)=>{
+        setSearchQuery(newValue)
+    }
+
+  return (
+    <SearchContext.Provider
+      value={{
+        searchQuery,
+        handleChangeSearchQuery
+      }}
+    >
+      {children}
+    </SearchContext.Provider>
+  );
+};
+
+export default SearchContextProvider;
