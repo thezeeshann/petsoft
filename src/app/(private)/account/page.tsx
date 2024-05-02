b63f67e16a683932cf28c0354f83689e81684ea4
thezeeshann <info.zeeshann@gmail.com>
2024-05-02 20:14:25 +0530

    refactor: updated dashboard components

--- src/app/(private)/account/page.tsx
+++ src/app/(private)/account/page.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import H1 from '@/components/h1'
+import ContentBlock from '@/components/content-block'
 
 const Account = () => {
   return (
-    <div>Account</div>
+    <main>
+      <H1 className='my-8 text-white'>You Account</H1>
+      <ContentBlock className="h-[500px] flex justify-center items-center">
+        <p>Logged in as....</p>
+      </ContentBlock>
+    </main>
   )
 }
 
